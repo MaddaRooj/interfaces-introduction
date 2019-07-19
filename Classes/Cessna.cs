7bfced4073cd6d158aab5fe2dd6aa387b6a19988
Addam Joor <38664958+MaddaRooj@users.noreply.github.com>
2019-07-18 19:13:33 -0500

    added generic typing to fueling stations

--- Classes/Cessna.cs
+++ Classes/Cessna.cs
@@ -1,7 +1,7 @@
 using System;
 
 namespace intro_interfaces {
-    public class Cessna : IGasPowered { // Propellor light aircraft
+    public class Cessna : IGasPowered {
         public double FuelCapacity { get; set; }
         public string MainColor { get; set; }
         public string MaximumOccupancy { get; set; }
